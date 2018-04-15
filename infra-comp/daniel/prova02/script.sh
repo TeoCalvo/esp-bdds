e0740d6b312ca732bae34354ce6d202898ec9443
Teo Calvo <teocalvo2@gmail.com>
2018-04-15 18:43:09 -0300

    Finalização da segunda prova de infra computacional

--- infra-comp/daniel/prova02/script.sh
+++ infra-comp/daniel/prova02/script.sh
@@ -5,18 +5,27 @@ opt=$2
 ext1=$3
 ext2=$4
 
+if [ $# -lt 4 ]; then
+    echo "Entre com os 4 argumentos corretamente"
+    exit
+
+elif [ ! -d $1 ]; then
+    echo "Entre com um diretório válido"
+    exit
+fi
+
 files=$(ls $diretorio | egrep "$ext1$")
 
-echo $opt
-
 if [ "$opt" = "-c" ]; then
-	for i in $files; do
-	    cp $diretorio/$i $diretorio/${i/$ext1/$ext2}
-	done
+    for i in $files; do
+        cp $diretorio/$i $diretorio/${i/$ext1/$ext2}
+    done
 
 elif [ "$opt" = "-m" ]; then
 	for i in $files; do
 	    mv $diretorio/$i $diretorio/${i/$ext1/$ext2}
 	done
-
+else
+    echo "Entre com uma opção para cópia ou mover válida"
+    exit
 fi
